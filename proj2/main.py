a6cb9e98c4ba8ef1894c9792fe6c04cfaeab49d8
João Silveira <joao.freixial.silveira@gmail.com>
2023-10-13 12:34:47 +0100

    Cleaned up code

--- proj2/main.py
+++ proj2/main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# alc23 - 2 - project1 
+# alc23 - 2 - project1
 # DO NOT remove or edit the lines above. Thank you.
 
-from typing import TextIO
-
 from z3 import *
+from typing import TextIO
 
 
 def parse(file: TextIO):
@@ -28,62 +27,83 @@ def parse(file: TextIO):
 
 
 def solve(required, stages, capacities, dependencies):
-    N_SWITCHES = len(stages)
     N_GROUPS = len(required)
+    N_SWITCHES = len(stages)
+
+    # Variables
+    groups = [
+        (Int(f"Group{i+1}'s switch"), Int(f"Group{i+1}'s stage "))
+        for i in range(N_GROUPS)
+    ]
+    switches = [Int(f"Switch{i+1}'s position") for i in range(N_SWITCHES)]
+
+    # Solver
     s = Optimize()
-    sws = [Int(f"SW {i+1} is in position") for i in range(N_SWITCHES)]
 
-    # groups[i][0] -> group's switch
-    # groups[i][1] -> group's stage
-    groups = [(Int(f"Group {i+1} is in switch"), Int(f"Group {i+1} is in stage ")) for i in range(N_GROUPS)]
-
-    # Each switch is in a single postion
     for i in range(N_SWITCHES):
-        s.add(And(sws[i] >= 0, sws[i] < N_SWITCHES))
-
-    # All switches are in different positions
-    s.add(Distinct(sws))
-
-    # Each group is in a single position
+        # Switches' position is valid
+        s.add(And(switches[i] >= 0, switches[i] < N_SWITCHES))
+    # Switches should be in distinct positions
+    s.add(Distinct(switches))
     for i in range(N_GROUPS):
+        # Group's switch is valid
         s.add(And(groups[i][0] >= 0, groups[i][0] < N_SWITCHES))
-
         for switch in range(N_SWITCHES):
-            s.add(And(groups[i][1] >= 0, \
-                                If(groups[i][0] == switch, groups[i][1] < stages[switch],True)))
-
-    # capacity constrains
+            # Group's stage must be valid
+            s.add(
+                And(
+                    groups[i][1] >= 0,
+                    If(groups[i][0] == switch, groups[i][1] < stages[switch], True),
+                )
+            )
     for switch in range(N_SWITCHES):
         for stage in range(stages[switch]):
-            s.add(Sum([If(And(groups[i][0] == switch, groups[i][1] == stage), \
-                                      required[i], 0) for i in range(N_GROUPS)]) <= capacities[switch])
-
-    # For each dependency (i, j), group i must be in a switch lower or equal than group j
-    for g1,g2 in dependencies:
+            # Each stage's capacity requirements should be met
+            s.add(
+                Sum(
+                    [
+                        If(
+                            And(groups[i][0] == switch, groups[i][1] == stage),
+                            required[i],
+                            0,
+                        )
+                        for i in range(N_GROUPS)
+                    ]
+                )
+                <= capacities[switch]
+            )
+    for g1, g2 in dependencies:
+        # There's no re-circulations between switches
         s.add(groups[g1][0] <= groups[g2][0])
 
     # Minimize recirculations
     cost = Int("cost")
-    s.add(cost == Sum([If(And( groups[g1][0] == groups[g2][0],
-                              groups[g1][1] >= groups[g2][1]), 1, 0) for g1,g2 in dependencies]))
+    s.add(
+        cost
+        == Sum(
+            [
+                If(
+                    And(groups[g1][0] == groups[g2][0], groups[g1][1] >= groups[g2][1]),
+                    1,
+                    0,
+                )
+                for g1, g2 in dependencies
+            ]
+        )
+    )
     s.minimize(cost)
 
-    if s.check() == sat:
-        model = s.model()
-        cost = model[cost].as_long()
-        sw = [0 for i in range(N_SWITCHES)]
-
-        for i in range(N_SWITCHES):
-            sw[model[sws[i]].as_long()] = i+1
-        
-        gp =  [[[] for i in range(stages[j])] for j in range(N_SWITCHES)]
-        for group,tup in enumerate(groups):
-            switch,stage = tup
-            gp[model[switch].as_long()][model[stage].as_long()].append(group+1)
-            
-        return cost, sw, gp
-    else:
+    if not s.check() == sat:
         return -1, [], []
+    else:
+        model = s.model()
+        sw = [0 for i in range(N_SWITCHES)]
+        for i in range(N_SWITCHES):
+            sw[model[switches[i]].as_long()] = i + 1
+        gp = [[[] for i in range(stages[j])] for j in range(N_SWITCHES)]
+        for group, (switch, stage) in enumerate(groups):
+            gp[model[switch].as_long()][model[stage].as_long()].append(group + 1)
+        return model[cost].as_long(), sw, gp
 
 
 def output(file: TextIO, cost, switches, groups):
