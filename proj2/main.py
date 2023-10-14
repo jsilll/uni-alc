0226af3be0dd9499e7b6e6eceb7102d907a35c43
João Silveira <joao.freixial.silveira@gmail.com>
2023-10-14 19:22:41 +0100

    Made new version

--- proj2/main.py
+++ proj2/main.py
@@ -40,9 +40,46 @@ def solve(required, stages, capacities, dependencies):
     # Optimizer
     s = Optimize()
     
-    # Optimizer settings
-    s.set("timeout", 10000)
-    s.set("maxsat_engine", "rc2")
+    # ctrl_c (bool) enable interrupts from ctrl-c (default: true)
+    # dump_benchmarks (bool) dump benchmarks for profiling (default: false)
+    # dump_models (bool) display intermediary models to stdout (default: false)
+    # elim_01 (bool) eliminate 01 variables (default: true)
+    # enable_sat (bool) enable the new SAT core for propositional constraints (default: true)
+    # lns_conflicts (unsigned int) initial conflict count for LNS search (default: 1000)
+    # maxlex.enable (bool) enable maxlex heuristic for lexicographic MaxSAT problems (default: true)
+    # maxres.max_core_size (unsigned int) break batch of generated cores if size reaches this number (default: 3)
+    # maxres.max_correction_set_size (unsigned int) allow generating correction set constraints up to maximal size (default: 3)
+    # maxres.max_num_cores (unsigned int) maximal number of cores per round (default: 200)
+    # maxres.pivot_on_correction_set (bool) reduce soft constraints if the current correction set is smaller than current core (default: true)
+    # pp.wcnf (bool) print maxsat benchmark into wcnf format (default: false)
+    # priority (symbol) select how to priortize objectives: 'lex' (lexicographic), 'pareto', 'box' (default: lex)
+    # rlimit (unsigned int) resource limit (0 means no limit) (default: 0)
+    # solution_prefix (symbol) path prefix to dump intermediary, but non-optimal, solutions (default: )
+    # timeout (unsigned int) timeout (in milliseconds) (UINT_MAX and 0 mean no timeout) (default: 4294967295)
+    # rc2.totalizer (bool) use totalizer for rc2 encoding (default: true)
+    # incremental (bool) set incremental mode. It disables pre-processing and enables adding constraints in model event handler (default: false)
+    # maxres.hill_climb (bool) give preference for large weight cores (default: true)
+
+    # enable_core_rotate (bool) enable core rotation to both sample cores and correction sets (default: false)
+    s.set("enable_core_rotate", True)
+    # enable_lns (bool) enable LNS during weighted maxsat (default: false)
+    s.set("enable_lns", True)
+    # enable_sls (bool) enable SLS tuning during weighted maxsat (default: false)
+    s.set("enable_sls", True)
+    # maxres.add_upper_bound_block (bool) restict upper bound with constraint (default: false)
+    s.set("maxres.add_upper_bound_block", True)
+    # maxres.maximize_assignment (bool) find an MSS/MCS to improve current assignment (default: false)
+    s.set("maxres.maximize_assignment", True)
+    # maxres.wmax (bool) use weighted theory solver to constrain upper bounds (default: false)
+    s.set("maxres.wmax", True)
+    # maxsat_engine (symbol) select engine for maxsat: 'core_maxsat', 'wmax', 'maxres', 'pd-maxres', 'maxres-bin', 'rc2' (default: maxres)
+    s.set("maxsat_engine", "maxres")
+    # optsmt_engine (symbol) select optimization engine: 'basic', 'symba' (default: basic)
+    s.set("optsmt_engine", "symba")
+    # pb.compile_equality (bool) compile arithmetical equalities into pseudo-Boolean equality (instead of two inequalites) (default: false)
+    s.set("pb.compile_equality", True)
+    # pp.neat (bool) use neat (as opposed to less readable, but faster) pretty printer when displaying context (default: true)
+    s.set("pp.neat", False)
 
     for i in range(N_SWITCHES):
         # Switches' position is valid
@@ -93,6 +130,8 @@ def solve(required, stages, capacities, dependencies):
     # Transform this into soft constraints
     for g1, g2 in dependencies:
         s.add_soft(Implies(groups[g1][0] == groups[g2][0], groups[g1][1] < groups[g2][1]))
+    
+    print(len(s.sexpr()), file=sys.stderr)
 
     if not s.check() == sat:
         return -1, [], []
